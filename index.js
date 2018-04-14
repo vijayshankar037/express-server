b286d846541b309a815a0041cee5ece5e203e69c
vijayshankar037 <vijayshankar1990@hotmail.com>
2018-04-14 14:06:56 +0530

    changes for herkou deployment

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const app = express();
 
-
+//Creating the / path
 app.get("/",(req,res) =>{
 	res.send({hi:"hello.."})
 });
 
+//defining the prot constant for express application.
+PORT = process.env.PORT || 5000;
 
-app.listen(5000);
+app.listen(PORT);
