f87ec5e9825312a654555a360c8f8b15a3dc708f
vijayshankar037 <vijayshankar1990@hotmail.com>
2018-04-28 12:06:13 +0530

    added the passport and GoogleStratergy to aap.js

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 const express = require('express');
+//
+const passport = require('passport');
+const GoogleStratergy = require('passport-google-oauth20').Strategy
+
 const app = express();
 
 //Creating the / path
-app.get("/",(req,res) =>{
-	res.send({hi:"hello.."})
-});
+// app.get("/",(req,res) =>{
+// 	res.send({hi:"hello.."})
+// });
+
+passport.use(new GoogleStratergy());
 
 //defining the prot constant for express application.
 PORT = process.env.PORT || 5000;
