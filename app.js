f7acd2ea70cde68981294a2540264c971666bbd7
vijayshankar037 <vijayshankar1990@hotmail.com>
2018-04-28 13:48:11 +0530

    Retrived the user profile form google and installed the nodemon

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const express = require('express');
-//
+//add the dependencies for google oauth
 const passport = require('passport');
 const GoogleStratergy = require('passport-google-oauth20').Strategy
-
+const keys = require('./config/keys');
 const app = express();
 
 //Creating the / path
@@ -10,8 +10,29 @@ const app = express();
 // 	res.send({hi:"hello.."})
 // });
 
-passport.use(new GoogleStratergy());
+passport.use(
+	new GoogleStratergy(
+	{
+        clientID: keys.googleClientID,
+        clientSecret: keys.googleClientSecret,
+        callbackURL: '/auth/google/callback'
+    },
+    (accessToken, refreshToken, profile, done) => {
+        console.log("Access Token:",accessToken);
+        console.log("refresh Token:",refreshToken);
+        console.log("profile:",profile);
+        console.log("Done:",done);
+    }
+));
 
+app.get(
+	'/auth/google',
+	passport.authenticate('google', {
+    	scope: ['profile', 'email']
+	}
+));
+
+app.get('/auth/google/callback',passport.authenticate('google'));
 //defining the prot constant for express application.
 PORT = process.env.PORT || 5000;
 
